d0e8abd935624521b3598d1a68bfc59047c2fe3f
Ivo Sisko <ivo.sisko@locastic.com>
2020-01-11 16:45:32 +0100

    changes

--- client/src/components/Layout/Main/Main.jsx
+++ client/src/components/Layout/Main/Main.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import "./Main.scss";
 
 const Main = ({ children, className }) => {
-  return <main className={`main ${className || ""}`}>{children} lolo</main>;
+  return (
+    <main className={`main ${className || ""}`}>{children}ldahsboard</main>
+  );
 };
 
 export default Main;
